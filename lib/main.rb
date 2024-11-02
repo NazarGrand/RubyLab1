3d1a4a1375d182f4b2203abaf9d8e082b225c86f
unknown <hryhoriak.nazarii@chnu.edu.ua>
2024-11-02 15:37:30 +0200

    added Cart class with flexible file saving options and custom output directory

--- lib/main.rb
+++ lib/main.rb
@@ -3,6 +3,7 @@ module DomRiaParserGrigoriakMelenkoMorar
   require_relative './app_config_loader'
   require_relative './logger_manager'
   require_relative './rental_item'
+  require_relative './cart'
 
   class Runner
     def self.run
@@ -20,6 +21,24 @@ module DomRiaParserGrigoriakMelenkoMorar
       rental_item = RentalItem.generate_fake
       LoggerManager.log_processed_file("Створено об'єкт RentalItem: #{rental_item.info}")
       puts rental_item.info
+
+      cart = Cart.new(config_data['output_dir'])
+      cart.generate_test_items(10)
+
+      puts "Items with price > 10000:"
+      cart.select { |item| item.price > 10000 }.each { |item| puts item.info }
+
+      puts "\nSorted items by price:"
+      cart.sort.each { |item| puts item.info }
+
+      total_price = cart.reduce(0) { |sum, item| sum + item.price }
+      puts "\nTotal price of all items: #{total_price} грн"
+
+      puts "\nSaving items to files..."
+      cart.save_to_file
+      cart.save_to_json
+      cart.save_to_csv
+      cart.save_to_yml
     end
   end
 end
